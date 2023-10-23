d6892db31babaf9dcf6af26842784c1d5b821f9a
QFranekQ <105975069+QFranekQ@users.noreply.github.com>
2023-10-23 19:42:45 +0200

    Lab-c :8ball:
    :100:

---
+++ src/Entity/Forecast.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForecastRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ForecastRepository::class)]
+class Forecast
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $temperature = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cloudy = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?float $humidity = null;
+
+    #[ORM\Column]
+    private ?float $chanceOfPrecipitation = null;
+
+    #[ORM\ManyToOne(inversedBy: 'forecasts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTemperature(): ?int
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(int $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getCloudy(): ?string
+    {
+        return $this->cloudy;
+    }
+
+    public function setCloudy(string $cloudy): static
+    {
+        $this->cloudy = $cloudy;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?float
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(float $humidity): static
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getChanceOfPrecipitation(): ?float
+    {
+        return $this->chanceOfPrecipitation;
+    }
+
+    public function setChanceOfPrecipitation(float $chanceOfPrecipitation): static
+    {
+        $this->chanceOfPrecipitation = $chanceOfPrecipitation;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+}

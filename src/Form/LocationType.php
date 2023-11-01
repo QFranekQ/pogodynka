494d91fc7306d1d5cc60bc46503502d4f9332d82
QFranekQ <105975069+QFranekQ@users.noreply.github.com>
2023-11-01 16:53:59 +0100

    Lab-d :+1:

---
+++ src/Form/LocationType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', null,[
+                'attr'=>[
+                    'placeholder'=>'eg. Szczecin',
+                ]
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland'=>'PL',
+                    'Germany'=>'DE',
+                    'France'=>'FR',
+                    'Spain'=>'ES',
+                    'Portugal'=>'PT',
+                    'United States'=>'US',
+                    'Italy'=>'IT',
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'attr' => [
+                    'min' => -90,
+                    'max' => 90,
+                ],
+                'html5'=>true,
+            ])
+            ->add('longtitude', NumberType::class, [
+                'attr' => [
+                    'min' => -180,
+                    'max' => 180,
+                ],
+                'html5'=>true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
